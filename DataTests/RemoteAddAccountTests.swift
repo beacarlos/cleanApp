7abddfd6327e7b22878a3f3125b970ab45526436
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2020-12-16 15:56:21 -0300

    ensure RemoteAddAccount calls HttpClient with correct data.

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Domain
 
 // resposabilidade de saber sobre a URL
 class RemoteAddAccount {
@@ -17,39 +18,58 @@ class RemoteAddAccount {
         self.httpClient = httpClient
     }
     
-    func add() {
-        httpClient.post(url: url)
+    func add(addAccountModel: AddAccountModel) {
+        let data = try? JSONEncoder().encode(addAccountModel)
+        httpClient.post(to: url, with: data)
     }
 }
 
 // protocolo que testa o metodo post do client, ou seja, cadastra uma conta.
 protocol HttpClientPost {
-    func post(url: URL)
+    func post(to url: URL, with data: Data?)
 }
 
-// pesquisa uma conta no client API.
-protocol HttpClientGet {
-    func get(url: URL)
-}
+//// pesquisa uma conta no client API.
+//protocol HttpClientGet {
+//    func get(to url: URL, with data: Data?)
+//}
 
 // teste que implementa a criação de uma conta nova utilizando API
 class RemoteAddAccountTests: XCTestCase {
+    // verifica se a URL de requisição está correta e faz o mocking da criação de uma conta.
     func test_add_should_call_httpClient_with_correct_url() {
         guard let url = URL(string: "http://any-url.com") else { return }
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
-        sut.add()
+        let addAccountModel = AddAccountModel(name: "Beatriz Carlos", email: "whobeatrizcarlos@gmail.com", password: "12345", passwordConfirmation: "12345")
+        sut.add(addAccountModel: addAccountModel)
         
         XCTAssertEqual(httpClientSpy.url, url)
+    }
+    
+    // teste que testa se o dado seja igual ao data abstraido pelo AddAccountModel.
+    func test_add_should_call_httpClient_with_correct_data() {
+        guard let url = URL(string: "http://any-url.com") else { return }
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        let addAccountModel = AddAccountModel(name: "Beatriz Carlos", email: "whobeatrizcarlos@gmail.com", password: "12345", passwordConfirmation: "12345")
+        sut.add(addAccountModel: addAccountModel)
+        
+        let data = try? JSONEncoder().encode(addAccountModel)
+        
+        XCTAssertEqual(httpClientSpy.data, data)
     }
 }
 
 extension RemoteAddAccountTests {
-    // mocking de um retorno de requiisição da API.
+    // class mocking de um retorno de requiisição da API.
     class HttpClientSpy: HttpClientPost {
         var url: URL?
-        func post(url: URL) {
+        var data: Data?
+        
+        func post(to url: URL, with data: Data?) {
             self.url = url
+            self.data = data
         }
     }
 }
