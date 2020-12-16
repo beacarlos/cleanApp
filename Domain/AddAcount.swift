cd0fdfc162aa635c8f6b37545806ce84574f7750
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2020-12-16 15:40:29 -0300

    transform domain use case and models to public

--- Domain/AddAcount.swift
+++ Domain/AddAcount.swift
@@ -8,16 +8,16 @@
 // caso de uso do domain
 import Foundation
 
-protocol AddAccount {
+public protocol AddAccount {
     // metodo que adiciona uma conta via api
     func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
 }
 
-struct AddAccountModel {
-    var name: String
-    var email: String
-    var password: String
-    var passwordConfirmation: String
+public struct AddAccountModel {
+    public var name: String
+    public var email: String
+    public var password: String
+    public var passwordConfirmation: String
 }
 
 
